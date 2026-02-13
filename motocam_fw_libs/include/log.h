37619b658a37aef1ea6dd5ae2b9a1058f7638119
Dhaval Shah <dhaval.s@outdu.com>
2026-02-13 16:28:43 +0530

    Updated log.h
    Signed-off-by: Dhaval Shah <dhaval.s@outdu.com>

--- motocam_fw_libs/include/log.h
+++ motocam_fw_libs/include/log.h
@@ -1,15 +1,37 @@
-#include <stdio.h>
-#include <stdarg.h>
 
+#include <cstdio>
+#include <source_location>
+
+template<typename... Args>
+void LOG_INFO(const char* fmt,
+              Args&&... args,
+              const std::source_location& loc = std::source_location::current())
+{
+    std::fprintf(stderr, "[INFO] %s(): ", loc.function_name());
+    std::fprintf(stderr, fmt, std::forward<Args>(args)...);
+    std::fprintf(stderr, "\n");
+}
+
+template<typename... Args>
+void LOG_ERROR(const char* fmt,
+               Args&&... args,
+               const std::source_location& loc = std::source_location::current())
+{
+    std::fprintf(stderr, "[ERROR] %s(): ", loc.function_name());
+    std::fprintf(stderr, fmt, std::forward<Args>(args)...);
+    std::fprintf(stderr, "\n");
+}
+
+template<typename... Args>
 #ifdef DEBUG
-    #define LOG_DEBUG(fmt, ...) \
-        fprintf(stderr, "[DEBUG] %s(): " fmt "\n", __func__, ##__VA_ARGS__)
+void LOG_DEBUG(const char* fmt,
+               Args&&... args,
+               const std::source_location& loc = std::source_location::current())
+{
+    std::fprintf(stderr, "[DEBUG] %s(): ", loc.function_name());
+    std::fprintf(stderr, fmt, std::forward<Args>(args)...);
+    std::fprintf(stderr, "\n");
+}    
 #else
     #define LOG_DEBUG(fmt, ...) ((void)0)
 #endif
-
-#define LOG_INFO(fmt, ...) \
-    fprintf(stderr, "[INFO] %s(): " fmt "\n", __func__, ##__VA_ARGS__)
-
-#define LOG_ERROR(fmt, ...) \
-  fprintf(stderr, "[ERROR] %s(): " fmt "\n", __func__, ##__VA_ARGS__)
